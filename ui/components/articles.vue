078590e7530117050b62e6d014f623702db082b7
Mehakpal <mehakpals@gmail.com>
2022-12-22 13:41:01 +0530

    Changed default value of showCreateArticle
    => Changed default value of showCreateArticle to false in Articels.vue

--- ui/components/articles.vue
+++ ui/components/articles.vue
@@ -170,6 +170,7 @@ export default {
       categories: [],
       path: routeName,
       articles: [] as Articles,
+      showCreateArticle: false,
       sortOption: 'asc',
       accessType: 'private'
     }
