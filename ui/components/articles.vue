c389ce730c1a003fa10476cf3be80843304a7a26
Mehakpal <mehakpals@gmail.com>
2022-12-23 10:08:13 +0530

    Removed extra variable
    => Removed extra variable from articels.(showCreateArticle)

--- ui/components/articles.vue
+++ ui/components/articles.vue
@@ -182,7 +182,6 @@ export default {
       categories: [],
       path: routeName,
       articles: [] as Articles,
-      showCreateArticle: false,
       sortOption: 'asc',
       accessType: 'private'
     }
